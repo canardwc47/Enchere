70453d5823bfad8f332a247c8c53ef629c7af732
Lucile FONLUPT <lucile.fonlupt2024@campus-eni.fr>
2025-02-04 12:25:31 +0100

    coucou les validations

--- src/main/java/fr/eni/projet/projeteni/bll/UtilisateurServiceImpl.java
+++ src/main/java/fr/eni/projet/projeteni/bll/UtilisateurServiceImpl.java
@@ -2,6 +2,8 @@ package fr.eni.projet.projeteni.bll;
 
 import fr.eni.projet.projeteni.bo.Utilisateur;
 import fr.eni.projet.projeteni.dal.UtilisateurDao;
+import fr.eni.projet.projeteni.exceptions.BusinessCode;
+import fr.eni.projet.projeteni.exceptions.BusinessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -45,4 +47,44 @@ public class UtilisateurServiceImpl implements UtilisateurService {
     public void deleteUtilisateur(Utilisateur utilisateur) {
         utilisateurDao.delete(utilisateur);
     }
+
+
+//    Validation d'un utilisateur
+
+    private boolean validerUtilisateur(Utilisateur u, BusinessException be) {
+        if (u == null) {
+            be.add(BusinessCode.VALIDATION_UTILISATEUR_NULL);
+            return false;
+        }
+        return true;
+    }
+
+//    Pseudo doit etre non nul, non vide, unique et ne contenir que des caractères alphanumérique
+
+    private boolean validerPseudo(String pseudo, BusinessException be) {
+        // Vérification de base : non null, non vide
+        if (pseudo == null || pseudo.isBlank() ) {
+            be.add(BusinessCode.VALIDATION_UTILISATEUR_PSEUDO_BLANK);
+            return false;
+        }
+        // Vérifie que le format soit valide
+        if(!pseudo.matches("^[a-zA-Z0-9_-]+$")){
+            be.add(BusinessCode.VALIDATION_UTILISATEUR_PSEUDO_INVALIDE);
+            return false;
+        }
+
+        // Vérifie que la taille du pseudo soit correct
+        if (pseudo.length() > 10 || pseudo.length() < 1) {
+            be.add(BusinessCode.VALIDATION_UTILISATEUR_PSEUDO_TAILLE);
+        }
+
+//        // Vérifier si le pseudo existe déjà en base
+//        if (utilisateurDao.findByPseudo(pseudo).isPresent()) {
+//            be.add(BusinessCode.VALIDATION_PSEUDO_DEJA_UTILISE);
+//            return false;
+//        }
+
+        return true; // Si tout est bon, pseudo valide
+    }
+
 }
