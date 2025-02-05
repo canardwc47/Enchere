4b6469556ad88326f075e331aef867ac49579185
Chapte-35 <sebastien.laloe@gmail.com>
2025-02-05 15:56:00 +0100

    commit fin projet spring secu ok resolutions de bugs

---
+++ src/main/java/fr/eni/projet/projeteni/bo/UserPrincipal.java
@@ -0,0 +1,53 @@
+package fr.eni.projet.projeteni.bo;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class UserPrincipal implements UserDetails {
+
+    private Utilisateur user;
+
+    public UserPrincipal(Utilisateur user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("ADMIN"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getMotDePasse();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
